ca937dd74ef29135ce953941823170995b577408
sid0433 <109646316+sid0433@users.noreply.github.com>
2022-07-22 12:10:11 +0530

    Update Doremon_Turtle.py
    removing code for cmmit demo

--- Doremon_Turtle.py
+++ Doremon_Turtle.py
@@ -1,35 +1,4 @@
-from turtle import *
-
-
-def my_goto(x, y):
-    penup()
-    goto(x, y)
-    pendown()
-
-
-def eyes():
-    fillcolor("#ffffff")
-    begin_fill()
-
-    tracer(False)
-    a = 2.5
-    for i in range(120):
-        if 0 <= i < 30 or 60 <= i < 90:
-            a -= 0.05
-            lt(3)
-            fd(a)
-        else:
-            a += 0.05
-            lt(3)
-            fd(a)
-    tracer(True)
-    end_fill()
-
-
-def beard():
-    my_goto(-32, 135)
-    seth(165)
-    fd(60)
+f
 
     my_goto(-32, 125)
     seth(180)
